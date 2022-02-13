935e6fbdfff2fb7e92f24785b0f7485ced6b09a1
Kami Kun <qewadsyt@gmail.com>
2022-02-14 06:08:17 +0800

    Started the base of the code

--- Index.js
+++ Index.js
@@ -1,1 +1,16 @@
-import config from "./Configurations/config.json"
+import { Client } from "yuuko";
+import path, { dirname } from "path";
+import 'dotenv/config'
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const client = new Client({
+    token: process.env.token,
+    prefix: process.env.prefix,
+    ignoreBots: true,
+})
+
+client
+    console.log(__dirname)
+    .connect();
